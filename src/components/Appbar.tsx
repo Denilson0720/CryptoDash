8f2fa04ee1365d16a71fa78107b69a169f2df1ee
denilson <denilson.felipe.25@gmail.com>
2024-09-03 11:25:45 -0400

    fix bug

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import {NavLink} from 'react-router-dom'
 
 interface Props {
